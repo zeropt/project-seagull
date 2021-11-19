999a2100eacdbd1e4af624d77382fe5af08b797a
zeropt <ferr0fluidmann@gmail.com>
2021-11-19 08:18:06 -0800

    Start p5 script

--- js/script.js
+++ js/script.js
@@ -3,3 +3,32 @@
  * Created: 16 NOV 2021
  * License: Public Domain
  */
+
+//Debug
+const DEBUG = true;
+
+//Global variables
+let canvas;
+
+/* Runs on startup */
+function setup() {
+    if (DEBUG) console.log("Running Setup!");
+
+    //Canvas Setup!
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0); //move canvas to the origin
+    if (DEBUG) console.log(
+        "Created New Canvas: (" + windowWidth + ", " + windowHeight + ")");
+}
+
+/* Runs on screen resize */
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    if (DEBUG) console.log(
+        "Canvas Resized: (" + windowWidth + ", " + windowHeight + ")");
+}
+
+/* Runs every cycle */
+function draw() {
+    background(200, 200, 255);
+}
